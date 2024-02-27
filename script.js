6364a3eb441936b296a0c37abca0515dd369935d
WidyaX1900 <tuderangga93@gmail,com>
2024-02-27 21:10:16 +0800

    Make counter and logotype inside the navbar

--- script.js
+++ script.js
@@ -9,3 +9,33 @@ trigger.addEventListener("click", () => {
   if (!isClose) ic.classList.replace("fa-bars", "fa-xmark");
   else ic.classList.replace("fa-xmark", "fa-bars");
 });
+
+counterAnimation("members", 1000, 0.1);
+counterAnimation("materials", 100, 10);
+counterAnimation("mentors", 80, 10);
+function counterAnimation(id, maxnumber, duration) {
+  let number = 0;
+  let maxNumber = maxnumber;
+  const numberEl = document.getElementById(id);
+
+  let interval = setInterval(() => {
+    if (number < maxNumber) {
+      number++;
+      if (number >= 1000) {
+        const nfObject = new Intl.NumberFormat("ID");
+        let result = nfObject.format(number);
+        if (number >= 1000000) {
+          let strResult = result.toString();
+          strResult = strResult.split(".");
+          numberEl.innerText = `${strResult[0]}M`;
+        } else {
+          numberEl.innerText = result;
+        }
+      } else {
+        numberEl.innerText = number;
+      }
+    } else {
+      clearInterval(interval);
+    }
+  }, duration);
+}
